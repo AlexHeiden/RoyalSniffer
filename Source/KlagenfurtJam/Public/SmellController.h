4cababe9d4b226d10773860ee3d80abbc4e71072
Aleksei Stepanov <prometey.titanov@gmail.com>
2025-06-01 01:31:32 +0200

    Fix setting rules to SmellController

--- Source/KlagenfurtJam/Public/SmellController.h
+++ Source/KlagenfurtJam/Public/SmellController.h
@@ -8,6 +8,61 @@
 #include "SmellController.generated.h"
 
 
+UCLASS(Blueprintable)
+class USmell : public UObject
+{
+	GENERATED_BODY()
+	
+	public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int color; // blue, yellow, red, green
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int shape; // cylinder, cone, sphere, cube
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int emission; // 0, 1, 2, 3
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int food; // cheese, meat, veggies
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool poisonous;
+
+	void Init(int newColor, int newShape, int newEmission, int newFood, bool newPoisonous) {
+		this->color = newColor;
+		this->shape = newShape;
+		this->emission = newEmission;
+		this->food = newFood;
+		this->poisonous = newPoisonous;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	bool getPoisonous() const {
+		return poisonous;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int getColor() const {
+		return color;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int getShape() const {
+		return shape;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int getEmission() const {
+		return emission;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int getFood() const {
+		return food;
+	}
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class KLAGENFURTJAM_API USmellController : public UActorComponent
 {
@@ -17,40 +72,84 @@ public:
 	// Sets default values for this component's properties
 	USmellController();
 	
-	int color; // blue, yellow, red, green
-	int shape; // cylinder, cone, sphere, cube
-	int emission; // 0, 1, 2, 3
-	int food; // cheese, meat, veggies
-	bool poisonous;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TArray<URule*> rules;
-
-	bool getPoisonous() const {
-		return poisonous;
-	}
-
-	int getColor() const {
-		return color;
-	}
-
-	int getShape() const {
-		return shape;
-	}
-
-	int getEmission() const {
-		return emission;
-	}
-
-	int getFood() const {
-		return food;
-	}
 
 	UFUNCTION(BlueprintCallable)
 	void setRules(TArray<URule*> newRules)
 	{
-		this->rules = rules;
+		for (int i = 0; i < 5; i++)
+		{
+			rules.Add(newRules[i]);
+		}
 	}
+
+	UFUNCTION(BlueprintCallable)
+	USmell* getNext() {
+        USmell* smell = NewObject<USmell>(this);
+		smell -> Init(-1, -1, -1, -1, false);
+
+        int rPoison = std::rand() % 4;
+
+        bool poisonous = (rPoison == 1);
+
+        smell->poisonous = poisonous;
+
+        if (poisonous) {
+            int ruleToComplyNum = std::rand() % 5;
+            URule* ruleToComply = rules[ruleToComplyNum];
+            for (int i = 0; i < 2; i++) {
+                switch (ruleToComply->condTypes[i])
+                {
+                    case 0:
+                        smell->color = ruleToComply->conditions[i];
+                        break;
+                    case 1:
+                        smell->shape = ruleToComply->conditions[i];
+                        break;
+                    case 2:
+                        smell->emission = ruleToComply->conditions[i];
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (smell->color == -1) {
+                smell->color = std::rand() % 4;
+            }
+            if (smell->shape == -1) {
+                smell->shape = std::rand() % 4;
+            }
+            if (smell->emission == -1) {
+                smell->emission = std::rand() % 4;
+            }
+            //smell.food to be added!
+        } else {
+            bool ok = false;
+            while (!ok) {
+                smell->color = std::rand() % 4;
+                smell->shape = std::rand() % 4;
+                smell->emission = std::rand() % 4;
+
+                ok = true;
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        if (rules[i]->condTypes[j] == 0 && rules[i]->conditions[j] == smell->color) {
+                            ok = false;
+                        }
+                        if (rules[i]->condTypes[j] == 1 && rules[i]->conditions[j] == smell->shape) {
+                            ok = false;
+                        }
+                        if (rules[i]->condTypes[j] == 2 && rules[i]->conditions[j] == smell->emission) {
+                            ok = false;
+                        }
+                    }
+                }
+            }
+        }
+		
+        return smell;
+    }
 
 protected:
 	// Called when the game starts
@@ -59,6 +158,4 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
 };
